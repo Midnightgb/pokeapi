e3e1958c540fa5f7469da5398db79ae3b5a9ffe9
Midnightgb <midnight3424@gmail.com>
2024-03-05 21:54:40 -0500

    Refactor code for Pokemon API integration

--- v2/dist/js/loader.js
+++ v2/dist/js/loader.js
@@ -2,5 +2,5 @@ window.onload = function () {
   loader = document.getElementById("loader");
   setTimeout(function() {
     loader.style.display = "none";
-  }, 5000); // 5000 milliseconds = 5 seconds
+  }, 1000);
 }
